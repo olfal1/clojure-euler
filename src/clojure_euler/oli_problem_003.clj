867411e9bfa0458d1cb68c714da875c424479ef4
Olivier Falardeau <ofalardeau@mirego.com>
2016-11-13 21:12:27 -0500

    faster solution than Erastosthene

--- src/clojure_euler/oli_problem_003.clj
+++ src/clojure_euler/oli_problem_003.clj
@@ -3,6 +3,7 @@
     [clojure.core]
     [clojure-euler.math-helper :as math]))
 
+; Eratosthene's Sieve (too slow for large numbers)
 (defn find-primes-in-range
   "Returns all prime numbers of the given collection"
   ([range]
@@ -14,10 +15,26 @@
              remaining-numbers (filter #(not (math/multiple? % (first remaining-numbers))) remaining-numbers)]
          (recur (into [] primes) (into [] remaining-numbers)))))))
 
+; faster solution
+(defn divisors
+  "Returns a list of the divisors of the given number"
+  [number]
+  (loop [current-divisor 2
+         divisors-list []]
+    (if (> current-divisor (long (Math/sqrt number)))
+      divisors-list
+      (if (math/multiple? number current-divisor)
+        (recur (inc current-divisor) (concat divisors-list (set [current-divisor (/ number current-divisor)])))
+        (recur (inc current-divisor) divisors-list)))))
+
+(defn prime?
+  "Returns wether a number is a prime number"
+  [number]
+  (empty? (divisors number)))
+
 (defn solution003
   "Finds the largest prime factor of 600851475143"
   []
-  (->> (range 3 600851475143 2)
-       (find-primes-in-range)
-       (filter #(math/multiple? 600851475143 %))
+  (->> (divisors 600851475143)
+       (filter #(prime? %))
        (apply max)))
