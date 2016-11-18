7fe253bec616f362480842d6ac7df0cf852312e2
sodominic <dominic.fillion.1@gmail.com>
2016-11-18 11:10:38 +0100

    Added Sodominic draft solution for problem 008 + refactored solutions to accept numbers higher than int max value

---
+++ src/clojure_euler/problem_008.clj
@@ -0,0 +1,7 @@
+(ns clojure-euler.problem-008
+  (:require [clojure-euler.sodominic.problem-008 :as sodominic]))
+
+(defn solution-008
+  "Returns highest product of 13 adjacent digits in 1000-digit the number"
+  []
+  (sodominic/solution-008))
--- test/clojure_euler/solutions_test.clj
+++ test/clojure_euler/solutions_test.clj
@@ -7,6 +7,7 @@
             [clojure-euler.problem-005 :refer :all]
             [clojure-euler.problem-006 :refer :all]
             [clojure-euler.problem-007 :refer :all]
+            [clojure-euler.problem-008 :refer :all]
             [clojure-euler.solutions :refer :all]))
 
 (deftest problem-001-test
@@ -36,3 +37,7 @@
 (deftest problem-007-test
   (testing "Solution 007"
     (is (= (get-solution "problem-007") (solution-007)))))
+
+(deftest problem-008-test
+  (testing "Solution 008"
+    (is (= (get-solution "problem-008") (solution-008)))))
