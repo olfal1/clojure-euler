a44b435ab473490c038cf29be1a3415e5cb1ff5f
Olivier Falardeau <ofalardeau@mirego.com>
2016-11-19 14:39:06 -0500

    solution of #5

--- src/clojure_euler/helpers/math_helper.clj
+++ src/clojure_euler/helpers/math_helper.clj
@@ -1,26 +1,26 @@
 (ns clojure-euler.helpers.math-helper)
 
 (defn multiple?
-  "Returns wheter the first number is a multiple of the second number"
+  "Returns wheter the first number is a multiple of the second number."
   [number divisor]
   (zero? (rem number divisor)))
 
 (defn exp [x n]
-  "Returns the result of x exp n"
+  "Returns the result of x exp n."
   (reduce * (repeat n x)))
 
-(defn factors
-  "Returns a list of the factors of the given number"
+(defn divisors
+  "Returns a list of the divisors of the given number."
   [number]
   (loop [current-number 1
-         factors-list []]
+         divisors []]
     (if (> current-number (long (Math/sqrt number)))
-      factors-list
+      divisors
       (if (multiple? number current-number)
-        (recur (inc current-number) (concat factors-list (set [current-number (/ number current-number)])))
-        (recur (inc current-number) factors-list)))))
+        (recur (inc current-number) (concat divisors (set [current-number (/ number current-number)])))
+        (recur (inc current-number) divisors)))))
 
 (defn prime?
-  "Returns whether a number is a prime number"
+  "Returns whether a number is a prime number."
   [number]
-  (= 2 (count (factors number))))
+  (= 2 (count (divisors number))))
--- src/clojure_euler/problem_005.clj
+++ src/clojure_euler/problem_005.clj
@@ -1,7 +1,9 @@
 (ns clojure-euler.problem-005
-  (:require [clojure-euler.sodominic.problem-005 :as sodominic]))
+  (:require
+    [clojure-euler.olfal1.problem-005 :as olfal1]
+    [clojure-euler.sodominic.problem-005 :as sodominic]))
 
 (defn solution-005
-  "Returns the smallest number divisible by all the numbers between 1 and 20"
+  "Returns the smallest number divisible by all the numbers between 1 and 20."
   []
-  (sodominic/solution-005))
+  (olfal1/solution-005))
