0052cb9a7e74520d275517bb75e91e8ba7a49859
Olivier Falardeau <ofalardeau@mirego.com>
2016-11-20 17:17:42 -0500

    new solution 5

--- src/clojure_euler/helpers/string_helper.clj
+++ src/clojure_euler/helpers/string_helper.clj
@@ -2,12 +2,5 @@
   (:require
     [clojure.string :as string]))
 
-(defn palindrome?
-  "Returns whether a string with even number of characters is a palindrome."
-  [string]
-  (let [number-size (count string)]
-    (if (even? number-size)
-      (let [first-half (subs string 0 (/ number-size 2))
-            second-half (subs string (/ number-size 2))]
-        (= first-half (string/reverse second-half)))
-      false)))
+(defn palindrome? [string]
+  (= (string/reverse string) string))
