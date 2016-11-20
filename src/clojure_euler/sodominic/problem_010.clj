ad5f1180e1aa2bdcd350365509259c96739bf85c
sodominic <dominic.fillion.1@gmail.com>
2016-11-20 22:18:51 +0100

    Corrected problem in sodominic's draft 008 + added sodominic's solution for problem 010

---
+++ src/clojure_euler/sodominic/problem_010.clj
@@ -0,0 +1,21 @@
+(ns clojure-euler.sodominic.problem-010
+  (:require [clojure-euler.helpers.math-helper :as math]))
+
+(defn solution-010
+  "Returns the sum of all primes below two million"
+  []
+  (loop [current-number 1
+         sum-of-primes 0]
+    (if (> current-number 2000000)
+      sum-of-primes
+      (if (math/prime? current-number)
+        (recur (inc current-number) (+ sum-of-primes current-number))
+        (recur (inc current-number) sum-of-primes)))))
+
+
+(defn solution-010-2
+  "Returns the sum of all primes below two million"
+  []
+  (->> (range 2000000)
+       (filter #(math/prime? %))
+       (reduce +)))
