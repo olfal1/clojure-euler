6a2acbd1cca391040bc4389b7ef3e65b26150a69
sodominic <dominic.fillion.1@gmail.com>
2016-11-21 14:26:52 +0100

    Small refactoring in problems 004 and 005

--- src/clojure_euler/problem_004.clj
+++ src/clojure_euler/problem_004.clj
@@ -3,24 +3,25 @@
     [clojure-euler.helpers.math-helper :as math]
     [clojure-euler.helpers.string-helper :as string]))
 
-(defn- between-100-and-999?
+(defn- three-digit?
+  "Returns true if the number is composed of three digits."
   [number]
   (and (>= number 100) (<= number 999)))
 
-(defn- product-of-two-3-digit?
-  "Returns true if the number is the product of two 3-digit numbers."
+(defn- product-of-two-three-digit?
+  "Returns true if the number is the product of two three-digit numbers."
   [number]
   (loop [current-number 100]
     (if (or (> current-number 999) (> current-number (long (Math/sqrt number))))
       false
-      (if (and (math/multiple? number current-number) (between-100-and-999? (/ number current-number)))
+      (if (and (math/multiple? number current-number) (three-digit? (/ number current-number)))
         true
         (recur (inc current-number))))))
 
 (defn solution-004
-  "Find the largest palindrome made from the product of two 3-digit numbers."
+  "Find the largest palindrome made from the product of two three-digit numbers."
   []
   (->> (range 998001 100001 -1)
        (filter #(string/palindrome? (str %)))
-       (filter product-of-two-3-digit?)
+       (filter product-of-two-three-digit?)
        (first)))
--- src/clojure_euler/problem_005.clj
+++ src/clojure_euler/problem_005.clj
@@ -1,17 +1,18 @@
 (ns clojure-euler.problem-005)
 
 (defn- greatest-common-divisor
-  [a b]
-  (if (zero? b)
-    a
-    (recur b (mod a b))))
+  "Returns the greatest common divisor of two numbers."
+  [first-number second-number]
+  (if (zero? second-number)
+    first-number
+    (recur second-number (mod first-number second-number))))
 
 (defn- least-common-multiple
-  [a b]
-  (/ (* a b) (greatest-common-divisor a b)))
+  "Returns the least common multiple of two numbers."
+  [first-number second-number]
+  (/ (* first-number second-number) (greatest-common-divisor first-number second-number)))
 
 (defn solution-005
+  "Returns the smallest positive number that is evenly divisible by all of the numbers from 1 to 20"
   []
-  (reduce
-    #(least-common-multiple %1 %2)
-    (range 1 21)))
+  (reduce least-common-multiple (range 1 21)))
